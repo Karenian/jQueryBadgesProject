201555ec61c5d3d27420260fd78611eac73279c5
karenian <karen422@gmail.com>
2017-01-08 22:30:51 -0800

    initial commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
-  // your code will go here
+    $.ajax({
+        url: 'https://www.codeschool.com/users/karenian.json',
+        dataType: 'jsonp',
+        success: function (response) {
+            var courses = response.courses.completed;
+            for(var i = 0; i < courses.length; i++)
+            {
+                $('#badges')
+                    .append($('<div>').addClass('course')
+                        .append($('<h3>').attr('title', courses[i].title))
+                        .append($('<img>').attr('src', courses[i].badge))
+                        .append($('<a>').attr({ href: courses[i].url, target: '_blank' }).addClass('btn btn-primary')));
+            }
+        }
+    });
 
 });
